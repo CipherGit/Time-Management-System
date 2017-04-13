f49db008e7b7dd164bc6b9d97261c0d4796988ad
Karl John Oberas <karl.oberas@obf.ateneo.edu>
2017-04-13 15:03:54 +0800

    finished search for friends

--- WebContent/js/jquery.js
+++ WebContent/js/jquery.js
@@ -6,4 +6,11 @@ $(document).ready(function(){
     $('#nav-here2').load('navbar2.html');
 });
 
-
+$('.panel-heading a').on('click',function(e){
+    if($(this).parents('.panel').children('.panel-collapse').hasClass('in')){
+        e.stopPropagation();
+    }
+    // You can also add preventDefault to remove the anchor behavior that makes
+    // the page jump
+    // e.preventDefault();
+});
