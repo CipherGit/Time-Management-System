b465a307185930264a39bb3931e85f2a197d5951
Karl John Oberas <karl.oberas@obf.ateneo.edu>
2017-04-14 20:41:26 +0800

    Show Members complete
    Working on Delete Members
    Note: add group owner to members list

--- src/servlets/ShowMembers.java
+++ src/servlets/ShowMembers.java
@@ -1,11 +1,19 @@
 package servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import database.AccountDBAO;
+import database.AccountDetails;
+import database.GroupDBAO;
 
 /**
  * Servlet implementation class ShowMembers
@@ -26,8 +34,34 @@ public class ShowMembers extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String group_id = request.getParameter("groupId");
+		String username = request.getParameter("username");
+		try {
+			GroupDBAO group = new GroupDBAO();
+			List<Integer> membersId = group.getUsersInGroup(Integer.parseInt(group_id));
+			AccountDBAO account = new AccountDBAO();
+			
+			response.setContentType("text/html;charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<div class=\"checkbox\">");
+			out.println("<div class=\"list-group\">");
+			for(int i=0; i<membersId.size(); i++) {
+				AccountDetails ad = new AccountDetails();
+				ad = account.getUserDetails(membersId.get(i));
+				out.println("<label class=\"list-group-item\" style=\"padding-left: 30px;\"><input type=\"checkbox\" name=\"members\" value=\""+ad.getUsername()+"\">");
+				out.println(ad.getName());
+				out.println("</label>");
+			}
+			out.println("</div>");
+			out.println("</div>");
+			out.println("<input name=\"group_id\" value=\""+group_id+"\" style=\"display:none;\">");
+			account.remove();
+			group.remove();
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**
